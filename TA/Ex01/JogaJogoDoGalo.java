d3ae62748e872073b7a59079180adcdf3889a5ec
funhete1 <51arthurm@gmial.com>
2021-05-25 12:29:50 +0100

    .

---
+++ TA/Ex01/JogaJogoDoGalo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import jogos.*;
+
+public class JogaJogoDoGalo {
+  public static final Scanner sin = new Scanner(System.in);
+  public static void main(String[] args) {
+    char jogador1 = 'X';
+    char jogador2 = 'O';
+    JogoDoGalo jogo = new JogoDoGalo(jogador1, jogador2);
+    jogo.mostraTabuleiro();
+    while (!jogo.terminado()) {
+      System.out.println("(lin col): ");
+      int lin = sin.nextInt();
+      int col = sin.nextInt();
+      jogo.jogada(lin, col);
+      jogo.mostraTabuleiro();
+    }
+    System.out.println();
+    if (!jogo.ultimoJogadorGanhou()) {
+        System.out.println("Jogador "+jogo.ultimoJogador()+" ganhou!");
+    } else {
+        System.out.println("Jogo empatado!");
+    }
+  }
+}
