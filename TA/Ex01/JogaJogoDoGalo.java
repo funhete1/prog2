0178eed9ab0cc44fe240c431b7c050768cea2946
funhete1 <51arthurm@gmial.com>
2021-05-26 21:07:11 +0100

    campeonato do galo

--- TA/Ex01/JogaJogoDoGalo.java
+++ TA/Ex01/JogaJogoDoGalo.java
@@ -3,23 +3,47 @@ import jogos.*;
 
 public class JogaJogoDoGalo {
   public static final Scanner sin = new Scanner(System.in);
+  
+  public static int counterx = 0;
+  public static int countero = 0; 
   public static void main(String[] args) {
+    
+    while(true){
+      char result = Galo();
+      if(result == 'X'){
+        counterx++;
+      }
+      else if(result=='O'){
+        countero++;
+      }
+      if(counterx == 3){
+        System.out.println("Jogador X ganhou");
+        break;
+      }
+      if(countero == 3){
+        System.out.println("Jogador O ganhou");
+        break;
+      }
+    }
+  
+  }
+  public static char Galo(){
     char jogador1 = 'X';
     char jogador2 = 'O';
     JogoDoGalo jogo = new JogoDoGalo(jogador1, jogador2);
     jogo.mostraTabuleiro();
-    while (!jogo.terminado()) {
+    System.out.printf("Placar:\n jogador X: %d\n jogador O: %d\n", counterx, countero);
+    while (!jogo.terminado()) { 
       System.out.println("(lin col): ");
       int lin = sin.nextInt();
       int col = sin.nextInt();
       jogo.jogada(lin, col);
       jogo.mostraTabuleiro();
     }
-    System.out.println();
-    if (jogo.ultimoJogadorGanhou()) {
-        System.out.println("Jogador "+jogo.ultimoJogador()+" ganhou!");
-    } else {
-        System.out.println("Jogo empatado!");
+    if(jogo.ultimoJogadorGanhou()){
+      return jogo.ultimoJogador();
     }
+    return 'a';
   }
 }
+
